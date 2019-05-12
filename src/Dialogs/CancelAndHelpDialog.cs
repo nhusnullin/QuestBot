9a0972dbd1e2b3235046ff38cca83c483dd17841
Sergey-Buyanov <>
2019-05-12 06:36:45 -0700

    # Some changes

--- src/Dialogs/CancelAndHelpDialog.cs
+++ src/Dialogs/CancelAndHelpDialog.cs
@@ -70,8 +70,8 @@ namespace CoreBot.Dialogs
                             await innerDc.Context.SendActivityAsync($"Выполнение квеста прервано");
                             return await innerDc.CancelAllDialogsAsync(cancellationToken);
                         case "2edaab42-9871-4c9b-8039-fd262121f8e0":
-                            await _teamService.DeleteTeams();
                             await _userService.DeleteUsers();
+                            await _teamService.DeleteTeams();
                             break;
 
                     }
