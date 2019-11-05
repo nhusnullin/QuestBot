f4745991752412d3a0e0c04b1c1314499b5e363d
Vadim Mingazhev <vadim.mingazhev@gmail.com>
2019-11-05 13:59:43 +0500

    Changed line break to work in telegram (1)

--- src/ScenarioBot/BotCommands/TopCommand.cs
+++ src/ScenarioBot/BotCommands/TopCommand.cs
@@ -43,7 +43,7 @@ namespace ScenarioBot.BotCommands
             var userWeights = await _userService.CalcUserWeightsAsync(userCount);
 
             var sb = new StringBuilder();
-            sb.Append($"Top {userCount} \r\n");
+            sb.Append($"Top {userCount} \n");
             
             foreach (var userWeight in userWeights) sb.Append($"{userWeight.Key} - {userWeight.Value} \r\n");
 
