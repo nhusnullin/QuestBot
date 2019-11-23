070862e93481d7f5f3e14e19d841bdd68d39b1a1
nhusnullin <nhusnullin@users.noreply.github.com>
2019-11-23 21:11:41 +0300

    Update publish.cmd

--- src/CoreBot/publish.cmd
+++ src/CoreBot/publish.cmd
@@ -1,3 +1,3 @@
 nuget restore
-msbuild CoreBot.sln -p:DeployOnBuild=true -p:PublishProfile=awesomedotnextquestbot-Web-Deploy.pubxml -p:Password=5aF0srpMma4ZMCBYXlvKmLDTicShC6oqwXp35RAo9p2jlKW8k0lMvonKLgRP
+msbuild CoreBot.sln -p:DeployOnBuild=true -p:PublishProfile=awesomedotnextquestbot-Web-Deploy.pubxml -p:Password=
 
